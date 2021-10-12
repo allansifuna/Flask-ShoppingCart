40b94d1f9dcddba1150e823eb8ea659da9e9dfca
allansifuna <allansifuna324@gmail.com>
2021-10-12 09:36:03 +0300

    fix: Typo

--- run.py
+++ run.py
@@ -114,20 +114,21 @@ def handle_cart():
     index = 0
     quantity_total = 0
 
-    for item in session['cart']:
-        product = Product.query.filter_by(id=item['id']).first()
+    if "cart" in session:
+        for item in session['cart']:
+            product = Product.query.filter_by(id=item['id']).first()
 
-        quantity = int(item['quantity'])
-        total = quantity * product.price
-        grand_total += total
+            quantity = int(item['quantity'])
+            total = quantity * product.price
+            grand_total += total
 
-        quantity_total += quantity
+            quantity_total += quantity
 
-        products.append({'id': product.id, 'name': product.name, 'price':  product.price,
-                         'image': product.image, 'quantity': quantity, 'total': total, 'index': index})
-        index += 1
+            products.append({'id': product.id, 'name': product.name, 'price':  product.price,
+                             'image': product.image, 'quantity': quantity, 'total': total, 'index': index})
+            index += 1
 
-    grand_total_plus_shipping = grand_total + 1000
+        grand_total_plus_shipping = grand_total + 1000
 
     return products, grand_total, grand_total_plus_shipping, quantity_total
 
